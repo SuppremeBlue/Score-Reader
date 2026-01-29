a96bc3b83f38155fb3931cadaec5e09d1a5e4733
SuppremeBlue <redpandac6@gmail.com>
2026-01-29 18:04:08 -0500

    Adding sample picture

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import cv2 as cv
+import sys
 
+image = "debussy_1"
+
+img = cv.imread(f"{image}.png")
+
+if img is None:
+    sys.exit("Unable to read image")
+
+cv.imshow("Display Window", img)
+k = cv.waitKey(0)
+if k == ord("s"):
+    cv.imwrite(f"processed_{image}.png",img)
